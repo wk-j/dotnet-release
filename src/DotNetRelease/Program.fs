2300375124d6df76836c3dd8b39e7f2e39eaab33
wk-j <somnuk.wk@gmail.com>
2018-02-01 03:15:52 +0700

    Created new release function

--- src/DotNetRelease/Program.fs
+++ src/DotNetRelease/Program.fs
@@ -1,89 +1,126 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open Octokit
+open System.IO
 
-type ReleaseOption = 
+type ReleaseOptions = 
     { Token: string
-      User: string
+      Owner: string
       Repository: string
-      Note: string
+      Body: string
       Tag: string
-      Title: string
+      Name: string
       Assets: string list
       Errors: string list
      }
 
 let defaultOptions = { 
     Token = ""
-    User =  ""
+    Owner = ""
     Repository = ""
-    Note = ""
+    Body = ""
     Tag = ""
-    Title = ""
+    Name = ""
     Assets = []
     Errors = []
 }
+
+module GitHub = 
+    let createReleaseo options = 
+        let client = GitHubClient(ProductHeaderValue("my-cool-app"))
+        client.Credentials <- Credentials(options.Token)
+
+        let release = NewRelease(options.Tag)
+        release.Name <- options.Name
+        release.Body <- options.Body
+        release.Prerelease <- false
+        release.Draft <- true
+
+        let newRelease  = 
+            client.Repository.Release.Create(options.Owner, options.Repository, release) 
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+
+        let uploadAsset release file = 
+            let info = FileInfo(file: string)
+            let raw = File.OpenRead(file)
+            let asset = ReleaseAssetUpload()
+            asset.FileName <- info.Name
+            asset.RawData <- raw
+
+            client.Repository.Release.UploadAsset(release, asset)
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+
+        options.Assets |> List.map (uploadAsset newRelease)
 
 let rec parseCommandLineRec args options = 
     let appendError error = 
         { options with Errors = error :: options.Errors }
 
     match args with
-    | [] -> 
-        options
+    | [] -> options
+
     | "--asset" :: xs ->
         match xs with
         | asset :: xss ->
             parseCommandLineRec xss { options with Assets = asset :: options.Assets  }
         | _ ->
-            appendError "--asset needs a value"
+            appendError "Option --asset needs a value"
             |> parseCommandLineRec xs
-    | "--note" :: xs ->
+
+    | "--body" :: xs ->
         match xs with
         | note :: xss  ->
-            parseCommandLineRec xss { options with Note = note }
+            parseCommandLineRec xss { options with Body = note }
         | _ ->
-            appendError "--note needs a value"
+            appendError "Option --body needs a value"
             |> parseCommandLineRec xs
-    | "--title" :: xs ->
+            
+    | "--name" :: xs ->
         match xs with
         | title :: xss ->
-            parseCommandLineRec xss { options with Title = title }
+            parseCommandLineRec xss { options with Name = title }
         | _ ->
-            appendError "--title needs a value"
+            appendError "Option --name needs a value"
             |> parseCommandLineRec xs
+
     | "--repository" :: xs ->
         match xs with
         | repo :: xss ->
             parseCommandLineRec xss { options with Repository = repo }
         | _ ->
-            appendError "--repository needs a value"
+            appendError "Option --repository needs a value"
             |> parseCommandLineRec xs 
+
     | "--token" :: xs ->
         match xs with
         | token::xss ->
             parseCommandLineRec xss { options with Token = token }
         | _ ->
-            appendError "--token needs a value"
+            appendError "Option --token needs a value"
             |> parseCommandLineRec xs
-    | "--user" :: xs ->
+
+    | "--owner" :: xs ->
         match xs with
         | user :: xss ->
-            parseCommandLineRec xss { options with User = user }
+            parseCommandLineRec xss { options with Owner = user }
         | _ ->
-            appendError "--user needs a value"
+            appendError "Option --owner needs a value"
             |> parseCommandLineRec xs
+
     | "--tag" :: xs ->
         match xs with
         | tag :: xss ->
             parseCommandLineRec xss { options with Tag = tag }
         | _ ->
-            appendError "--tag needs a value"
+            appendError "Option --tag needs a value"
             |> parseCommandLineRec xs
+
     | x :: xs ->
         appendError (sprintf "Option %s is unrecognized " x)
         |> parseCommandLineRec xs
 
 [<EntryPoint>]
 let main argv =
-    let rs = parseCommandLineRec ["--tag"; "0.1.0"; "--user"; "wk-j"] defaultOptions
+    let rs = parseCommandLineRec (argv |> Array.toList) defaultOptions
     printfn "%A" rs
     0 // return an integer exit code
